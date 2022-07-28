83f40b10c7e1e724e870f910fae20b2490303011
Yulia Ye <yuliayqy@gmail.com>
2022-07-28 19:19:08 +0800

    change crosschain

--- pages/faucet/-components/CrossChain.vue
+++ pages/faucet/-components/CrossChain.vue
@@ -28,85 +28,98 @@
   </div>
 </template>
 
-<script setup>
+<script>
 import { ref } from 'vue'
 import Web3 from 'web3'
 import { ABI } from './contract.ts'
-import { ApiPromise, WsProvider } from '@polkadot/api'
 
-const ercAmount = ref('')
-const ercAddress = ref('')
+export default {
+  async setup() {
+    if (!process.client) return {}
 
-const {
-  web3Accounts,
-  web3Enable,
-  web3FromAddress,
-} = await import('@polkadot/extension-dapp')
+    const ercAmount = ref('')
+    const ercAddress = ref('')
 
-const handlerPolkadot = async function () {
-  const wsProvider = new WsProvider('wss://ws.test.hamsternet.io')
-  const api = await ApiPromise.create({ provider: wsProvider })
+    const {
+      web3Accounts,
+      web3Enable,
+      web3FromAddress,
+    } = await import('@polkadot/extension-dapp')
+    const {
+      ApiPromise,
+      WsProvider
+    } = await import('@polkadot/api')
 
-  const allInjected = await web3Enable('my cool dapp')
-  const allAccounts = await web3Accounts()
-  console.log('allAccounts',allAccounts)
-  if (allAccounts.length === 0) {
-    alert('you need init an polkadot account')
-    return
-  }
-  const SENDER = allAccounts[0].address
+    const handlerPolkadot = async function () {
+      const wsProvider = new WsProvider('wss://ws.test.hamsternet.io')
+      const api = await ApiPromise.create({ provider: wsProvider })
 
-  const injector = await web3FromAddress(SENDER);
-  api.tx.burn.burn(ercAmount.value*1, ercAddress.value).signAndSend(
-    SENDER, {signer: injector.signer}, (result) => {
-      if (result.status.isInBlock) {
-        console.log(`Transaction included at blockHash ${result.status.asInBlock}`);
-      } else if (result.status.isFinalized) {
-        console.log(`Transaction finalized at blockHash ${result.status.asFinalized}`);
+      const allInjected = await web3Enable('my cool dapp')
+      const allAccounts = await web3Accounts()
+      console.log('allAccounts',allAccounts)
+      if (allAccounts.length === 0) {
+        alert('you need init an polkadot account')
+        return
+      }
+      const SENDER = allAccounts[0].address
+
+      const injector = await web3FromAddress(SENDER);
+      api.tx.burn.burn(ercAmount.value*1, ercAddress.value).signAndSend(
+        SENDER, {signer: injector.signer}, (result) => {
+          if (result.status.isInBlock) {
+            console.log(`Transaction included at blockHash ${result.status.asInBlock}`);
+          } else if (result.status.isFinalized) {
+            console.log(`Transaction finalized at blockHash ${result.status.asFinalized}`);
+          }
+        }
+      )
+    }
+
+    const connected = ref(false)
+    const polkaAmount = ref('')
+    const polkaAddress = ref('')
+
+    const hookWeb3 = function () {
+      if (window.ethereum) {
+        window.ethereum.request({ method: 'eth_requestAccounts' })
+          .then(() => {
+            connected.value = true
+          })
+      } else {
+        alert('请安装MateMask')
       }
     }
-  )
-}
 
-const connected = ref(false)
-const polkaAmount = ref('')
-const polkaAddress = ref('')
+    const transformPolkadot = async function() {
+      const wsProvider = new WsProvider('wss://polkadot.authright-test.newtouch.com')
+      const api = await ApiPromise.create({ provider: wsProvider })
+      await hookWeb3()
 
-const hookWeb3 = function () {
-  if (window.ethereum) {
-    window.ethereum.request({ method: 'eth_requestAccounts' })
-      .then(() => {
-        connected.value = true
+      const web3 = new Web3(window.ethereum)
+      console.log("web3",web3)
+      const accounts = await web3.eth.getAccounts()
+      console.log('accounts', accounts)
+      const contract = new web3.eth.Contract(ABI, '0x58DC15156C520cB4d18Df8807419c1989B05c960')
+      console.log('contract',contract)
+      contract.methods.burn(polkaAmount.value*1, polkaAddress.value).send({
+        from: accounts[0]
+      }).on('transactionHash', function (hash) {
+        console.log('transactionHash:', hash)
+      }).on('confirmation', function (confirmationNumber, receipt) {
+        console.log('confirmation:', confirmationNumber)
+      }).on('receipt', function (receipt) {
+        // receipt example
+        console.log('receipt:', receipt)
+      }).on('error', function (error) {
+        console.log('error:', error)
       })
-  } else {
-    alert('请安装MateMask')
+    }
+
+    return {
+      handlerPolkadot,transformPolkadot,ercAmount,ercAddress,polkaAmount,polkaAddress
+    }
+
   }
-}
-
-const transformPolkadot = async function() {
-  const wsProvider = new WsProvider('wss://polkadot.authright-test.newtouch.com')
-  const api = await ApiPromise.create({ provider: wsProvider })
-  await hookWeb3()
-
-  const web3 = new Web3(window.ethereum)
-  console.log("web3",web3)
-  const accounts = web3.eth.accounts
-  // const accounts = await web3.eth.getAccounts()
-  console.log('accounts', accounts)
-  const contract = new web3.eth.Contract(ABI, '0x58DC15156C520cB4d18Df8807419c1989B05c960')
-  console.log('contract',contract)
-  contract.methods.burn(polkaAmount.value*1, polkaAddress.value).send({
-    from: accounts[0]
-  }).on('transactionHash', function (hash) {
-    console.log('transactionHash:', hash)
-  }).on('confirmation', function (confirmationNumber, receipt) {
-    console.log('confirmation:', confirmationNumber)
-  }).on('receipt', function (receipt) {
-    // receipt example
-    console.log('receipt:', receipt)
-  }).on('error', function (error) {
-    console.log('error:', error)
-  })
 }
 
 </script>
