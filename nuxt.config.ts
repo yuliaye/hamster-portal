5ee4437944f3b53d8d51106e3b62e966bc12801b
mohaijiang <mohaijiang110@163.com>
2022-09-23 22:12:25 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,8 +35,8 @@ export default defineNuxtConfig({
       },
       hamsterProviderPackage: {
         version: 'v1.3.0',
-        windowsLink: 'https://github.com/hamster-shared/hamster-provider/releases/download/v1.3.0/hamster-provider-v1.3.0-windows-amd64.zip',
-        macLink: 'https://github.com/hamster-shared/hamster-provider/releases/download/v1.3.0/hamster-provider-v1.3.0-darwin-amd64.tar.gz',
+        windowsLink: 'https://github.com/hamster-shared/hamster-provider/releases/download/v1.3.1/hamster-provider-v1.3.1-windows-amd64.zip',
+        macLink: 'https://github.com/hamster-shared/hamster-provider/releases/download/v1.3.1/hamster-provider-v1.3.1-darwin-amd64.zip',
         linuxLink: 'https://github.com/hamster-shared/hamster-provider/releases/download/v1.3.0/hamster-provider-v1.3.0-linux-amd64.tar.gz'
       }
     }
